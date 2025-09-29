871f0b450fe2cf16e12cc49d0fbf56d26c6080fe
dendefo <daniek332@gmail.com>
2025-09-29 23:41:53 +0300

    links

--- script.js
+++ script.js
@@ -12,17 +12,35 @@ const section = document.createElement("section");
 section.className = "section";
 
 
-section.innerHTML = `
-<div class="text">
-<h2>${item.Title}</h2>
-<p>${item.Description}</p>
-</div>
-<div class="video">
-<iframe src="${item.VideoLink}" allowfullscreen></iframe>
-</div>
-`;
+        // Create platform links HTML
+        let platformLinks = '';
+        if (item.AndroidLink) {
+            platformLinks += `
+                <a href="${item.AndroidLink}" target="_blank" class="platform-badge">
+                    <img src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" alt="Get it on Google Play" class="store-badge">
+                </a>
+            `;
+        }
+        if (item.IOSLink) {
+            platformLinks += `
+                <a href="${item.IOSLink}" target="_blank" class="platform-badge">
+                    <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="Download on the App Store" class="store-badge">
+                </a>
+            `;
+        }
 
-
+        section.innerHTML = `
+            <div class="text">
+                <h2>${item.Title}</h2>
+                <p>${item.Description}</p>
+                <div class="platform-links">
+                    ${platformLinks}
+                </div>
+            </div>
+            <div class="video">
+                <iframe src="${item.VideoLink}" allowfullscreen></iframe>
+            </div>
+        `;
 container.appendChild(section);
 });
 } catch (error) {
