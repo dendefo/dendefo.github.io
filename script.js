73323cb8d133334a65b1cb212ef17a3b9f71721a
dendefo <daniek332@gmail.com>
2025-09-30 00:24:18 +0300

    More projects

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const container = document.getElementById("portfolio-container");
 
 
 try {
-const response = await fetch("data.json");
-const projects = await response.json();
-
-
-projects.forEach(item => {
-const section = document.createElement("section");
-section.className = "section";
-
-
+    const response = await fetch("data.json");
+    const projects = await response.json();
+    
+    
+    projects.forEach(item => {
+        const section = document.createElement("section");
+        section.className = "section";
+        const subtitleHTML = item.SubTitle ? `<h3 class="subtitle">${item.SubTitle}</h3>` : '';
+        
+        
+        
         // Create platform links HTML
         let platformLinks = '';
         if (item.AndroidLink) {
@@ -28,24 +30,41 @@ section.className = "section";
                 </a>
             `;
         }
-
+        if (item.GoogleDriveLink) {
+            platformLinks += `
+                <a href="${item.GoogleDriveLink}" target="_blank" class="platform-badge google-drive-badge">
+                    <img src="https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png" alt="Google Drive" class="drive-icon">
+                    <span class="drive-text">View on Google Drive</span>
+                </a>
+            `;
+        }
+        var videoLinkPart = item.VideoLink? `
+        <div class="video">
+                <iframe src="${item.VideoLink}" allowfullscreen></iframe>
+            </div>
+            ` : '';
+            var imageLinkPart = item.ImageLink? `
+            <div class="image">
+                    <img src="${item.ImageLink}" alt="${item.Title} Image"/>
+                </div>
+                ` : '';
         section.innerHTML = `
             <div class="text">
                 <h2>${item.Title}</h2>
+                    ${subtitleHTML}
                 <p>${item.Description}</p>
                 <div class="platform-links">
                     ${platformLinks}
                 </div>
             </div>
-            <div class="video">
-                <iframe src="${item.VideoLink}" allowfullscreen></iframe>
-            </div>
+            ${videoLinkPart}
+            ${imageLinkPart}
         `;
-container.appendChild(section);
-});
+        container.appendChild(section);
+    });
 } catch (error) {
-container.innerHTML = "<p>Failed to load portfolio data.</p>";
-console.error(error);
+    container.innerHTML = "<p>Failed to load portfolio data.</p>";
+    console.error(error);
 }
 }
 
